008765bf0bf5cbd8f6356e733c6947d0ef400876
Andrej <andrzejapfelbaum@gmail.com>
2022-09-12 18:44:13 +0300

    Add two new features

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
+let color = "black";
 function fillBoard(squares) {
+
 for (let i = 1; i <= squares; i++) {
    const sketch = document.querySelector('#sketch');
    const div = document.createElement('div');
-   div.classList.add('square');
-   div.addEventListener('mouseover', mouseOver);
-   //div.addEventListener('mouseout', mouseOut);
-
+   div.addEventListener('mouseover', colorSquare);
    sketch.appendChild(div);
-  
 }
 }
 
 fillBoard(256);
 
-
-
-function mouseOver(e) {
-   this.style.backgroundColor = 'black';
-   
+function colorSquare(e) {
+   if (color === "gray") {
+      let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+      if (currentOpacity <= 0.9) {
+          this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+          }}
+    else if (color === "random") {
+      let rgb1 = Math.floor(Math.random() * 255);
+      let rgb2 = Math.floor(Math.random() * 255);
+      let rgb3 = Math.floor(Math.random() * 255);
+      let randomColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+      this.style.backgroundColor = randomColor;
+   }
+   else {
+   this.style.backgroundColor = color;   
+   }
 }
 
+const newGrid = document.querySelector('.newGrid');
+newGrid.addEventListener('click', setNewGrid);
 
+function setNewGrid() {
 
-function mouseOut(e) {
-   this.style.backgroundColor = 'white';
-   
-}
-
-
-
-const button = document.querySelector('button');
-button.addEventListener('click', newGrid);
-
-function newGrid() {
-   
    let result;
    const sketch = document.querySelector('#sketch');
    do {
-    
     result = prompt('Enter the number of squares per side for the new grid (up to 100)');
    }
    while ((result > 100) || (result < 1)); 
@@ -52,9 +51,11 @@ function newGrid() {
    let squares = result * result;
    
    fillBoard(squares);
-   
-   
-
 }
+
+function changeColor(chosenColor) {
+   color = chosenColor;
+}
+
 
 
